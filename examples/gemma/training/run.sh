66bb1a9440ab8fcc3d0764a036416ea2ec94f815
Nidhi Hiremath <nidhi.hiremath@baseten.co>
2025-08-20 12:08:25 -0700

    remove comments

--- examples/gemma/training/run.sh
+++ examples/gemma/training/run.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 set -eux
 
-# nvidia-smi
-
-# pip3 uninstall -y axolotl
-# pip3 install -U packaging setuptools wheel ninja
-
-# git clone https://github.com/axolotl-ai-cloud/axolotl
-# cd axolotl
-
-# # Install in development mode with your desired extras
-# pip3 install -e .[flash-attn,deepspeed,ring-flash-attn]
 pip3 install ring-flash-attn>=0.1.4 #new, along with ^ ring-flash-attn
 pip3 install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@7f6afce"
 
 sed -i 's/grad_scale = 1 \/ lse\.numel()/grad_scale = 1 \/ lse\.numel() if lse\.numel() else 1\.0/' /root/miniconda3/envs/py3.11/lib/python3.11/site-packages/cut_cross_entropy/cce.py
-# cd ..
 
 export NCCL_SOCKET_IFNAME="^docker0,lo"
 export NCCL_IB_DISABLE=0
 export NCCL_TIMEOUT=1800000
 
-huggingface-cli login --token=$HF_TOKEN # hf auth login?
+huggingface-cli login --token=$HF_TOKEN
 
 axolotl preprocess config.yaml
 
 torchrun --nnodes=$BT_GROUP_SIZE --nproc-per-node=$BT_NUM_GPUS --node-rank=$BT_NODE_RANK --rdzv-backend=c10d --rdzv-id=$BT_TRAINING_JOB_ID --rdzv-endpoint=$BT_LEADER_ADDR:29400  -m axolotl.cli.train config.yaml
-
-# sleep infinity
