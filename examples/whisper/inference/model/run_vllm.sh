e5c532a136e9b5e53289d2db977fae05fa10a6f4
Nidhi Hiremath <nidhi.hiremath@baseten.co>
2025-08-26 15:33:45 -0700

    add example to run inference

--- examples/whisper/inference/model/run_vllm.sh
+++ examples/whisper/inference/model/run_vllm.sh
@@ -1,4 +1,4 @@
 export CHECKPOINT_HF=baseten-admin/whisper-larger-v3-turbo-minirun3
 
-# vllm serve $CHECKPOINT_HF --served-model-name gemma3 --max-model-len 32000 --tensor-parallel-size 2 --gpu-memory-utilization 0.8 --trust-remote-code --dtype bfloat16
+# vllm serve $CHECKPOINT_HF --served-model-name whisper-larger-v3-turbo-ft3 --max-model-len 32000 --tensor-parallel-size 2 --gpu-memory-utilization 0.8 --trust-remote-code --dtype bfloat16
 vllm serve $CHECKPOINT_HF --dtype auto
---
+++ examples/whisper/inference/run_inference_example.py
@@ -0,0 +1,17 @@
+## TODO: update these with your deployment details
+API_KEY = "" 
+BASE_URL = "https://model-4q9l8ejq.api.baseten.co/development/sync/v1"
+MODEL_NAME = "baseten-admin/whisper-larger-v3-turbo-minirun3"
+
+from openai import OpenAI
+
+print(f"Loading audio file and sending to model for transcription...")
+audio_file = open("speech-94649.mp3", "rb")
+
+print("Sending request to model...")
+client = OpenAI(
+    api_key=API_KEY,
+    base_url=BASE_URL,
+)
+transcription = client.audio.transcriptions.create(model=MODEL_NAME, file=audio_file)
+print(transcription)
