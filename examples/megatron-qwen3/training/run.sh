66514e8e7851d54af3a0131dc0958de0cf668bfd
Nidhi Hiremath <nidhi.hiremath@baseten.co>
2025-09-17 18:12:53 -0700

    convert ckpt first

--- examples/megatron-qwen3/training/run.sh
+++ examples/megatron-qwen3/training/run.sh
@@ -10,17 +10,20 @@ git clone https://github.com/NVIDIA/Megatron-LM.git Megatron-LM --branch core_r0
 
 cd /root/
 export DATASET="zai-org/LongAlign-10k"
-export MODEL_ID="Qwen/Qwen3-30B-A3B"
+export MODEL_ID="Qwen/Qwen3-30B-A3B-Instruct-2507"
 
-### Set output directory to BT_CHECKPOINT_DIR if it exists, otherwise use some dir
-if [ -n "$BT_CHECKPOINT_DIR" ] && [ -d "$BT_CHECKPOINT_DIR" ]; then
-    OUTPUT_DIR="$BT_CHECKPOINT_DIR"
-else
-    OUTPUT_DIR="outputs"
-fi
+export MCORE_MODEL_DIR="Converted/Qwen3-30B-A3B-Instruct-2507-mcore"
+swift export \
+    --model $MODEL_ID \
+    --to_mcore true \
+    --torch_dtype bfloat16 \
+    --use_hf \
+    --output_dir $MCORE_MODEL_DIR
+
+echo "Done converting ckpt"
 
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True NPROC_PER_NODE=8 NNODES=$BT_GROUP_SIZE NODE_RANK=$BT_NODE_RANK MASTER_ADDR=$BT_LEADER_ADDR megatron sft \
-    --model $MODEL_ID \
+    --load $MCORE_MODEL_DIR \
     --dataset $DATASET \
     --no_initialization false \
     --split_dataset_ratio 0.01 \
@@ -38,13 +41,13 @@ PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True NPROC_PER_NODE=8 NNODES=$BT_GRO
     --recompute_method uniform \
     --recompute_num_layers 4 \
     --train_iters 200 \
-    --eval_iters 50 \
+    --eval_iters 40 \
     --finetune true \
     --cross_entropy_loss_fusion true \
     --lr 1e-5 \
     --lr_warmup_fraction 0.05 \
     --min_lr 1e-6 \
-    --save /app/checkpoints \
+    --save $BT_CHECKPOINT_DIR \
     --eval_interval 40 \
     --save_interval 40 \
     --max_length 32000 \
@@ -56,4 +59,6 @@ PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True NPROC_PER_NODE=8 NNODES=$BT_GRO
     --attention_backend flash \
     --optimizer_cpu_offload true \
     --use_precision_aware_optimizer true \
-    --use_hf
+    --use_hf 1 \
+    --wandb_project qwen3_moe_megatron \
+    --wandb_exp_name all_training_b10 \
