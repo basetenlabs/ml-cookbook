52eac52bc5669d66831e38ae97bd78619a9844e4
Nidhi Hiremath <nidhi.hiremath@baseten.co>
2025-10-30 13:59:48 -0700

    dpo v1

---
+++ examples/qwen3-8b-lora-dpo-trl/training/config.py
@@ -0,0 +1,40 @@
+from truss_train import definitions
+from truss.base import truss_config
+
+BASE_IMAGE = "pytorch/pytorch:2.7.0-cuda12.8-cudnn9-runtime"
+PROJECT_NAME = "Qwen3-8b-lora-dpo-trl"
+
+training_runtime = definitions.Runtime(
+    start_commands=["/bin/sh -c 'chmod +x ./run.sh && ./run.sh'"],
+    environment_variables={
+        "HF_TOKEN": definitions.SecretReference(
+            name="hf_access_token"
+        ),  # The name of the HF Access Token secret in your B10 account
+        "WANDB_API_KEY": definitions.SecretReference(
+            name="wandb_api_key"
+        ),  # comment this out if you don't want to use wandb
+        "WANDB_PROJECT": "qwen3-8b-lora-dpo",
+    },
+    cache_config=definitions.CacheConfig(
+        enabled=True,
+    ),
+)
+
+training_compute = definitions.Compute(
+    accelerator=truss_config.AcceleratorSpec(
+        accelerator=truss_config.Accelerator.H100,
+        count=8,
+    ),
+    node_count=1,  
+)
+
+training_job = definitions.TrainingJob(
+    image=definitions.Image(base_image=BASE_IMAGE),
+    compute=training_compute,
+    runtime=training_runtime,
+)
+
+training_project = definitions.TrainingProject(
+    name=PROJECT_NAME, job=training_job
+)
+
