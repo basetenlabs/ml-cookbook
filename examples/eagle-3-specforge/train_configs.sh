fafbf2dc97c20794278c23848d016eb730249dea
ervinwang-jpg <ervin.wang@baseten.co>
2026-01-11 22:07:18 -0500

    Update train_configs.sh

--- examples/eagle-3-specforge/train_configs.sh
+++ examples/eagle-3-specforge/train_configs.sh
@@ -3,32 +3,30 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 export TORCHINDUCTOR_CACHE_DIR=$ROOT_DIR/cache/compiled_kernels
-
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-NUM_GPUS=4
+BT_TEAM_CACHE_DIR=${BT_TEAM_CACHE_DIR:? "BT_TEAM_CACHE_DIR env var must be set"}
+# support tp8 train eagle3 for Qwen3-4B/8B/32B up to tp_size = 8
+NUM_GPUS=${1:-8}
+TP_SIZE=${1:-8}
 BUILD_DATASET_NUM_PROC=${BUILD_DATASET_NUM_PROC:-64}
 
 torchrun \
     --standalone \
     --nproc_per_node $NUM_GPUS \
     $ROOT_DIR/scripts/train_eagle3.py \
-    --target-model-path Qwen/Qwen3-Coder-30B-A3B-Instruct \
-    --draft-model-config $ROOT_DIR/configs/qwen3-coder-30B-A3B-instruct-eagle3.json \
-    --train-data-path $ROOT_DIR/cache/dataset/opc_train.jsonl \
+    --target-model-path Qwen/Qwen3-4B \
+    --draft-model-config $ROOT_DIR/configs/qwen3-4b-eagle3.json \
+    --train-data-path $ROOT_DIR/cache/dataset/perfectblend_train_regen.jsonl \
     --build-dataset-num-proc $BUILD_DATASET_NUM_PROC \
-    --output-dir $ROOT_DIR/outputs/qwen3-coder-30b-a3b-instruct-eagle3-opc-train \
-    --num-epochs 2 \
+    --output-dir $ROOT_DIR/outputs/qwen3-4b-eagle3-perfectblend-ttt-9-regen \
+    --num-epochs 10 \
     --batch-size 1 \
+    --ttt-length 9 \
     --learning-rate 1e-4 \
-    --max-length 4096 \
+    --max-length 2048 \
+    --save-interval 50000 \
+    --eval-interval 50000 \
     --chat-template qwen \
     --cache-dir $ROOT_DIR/cache \
     --embedding-key model.embed_tokens.weight \
-    --tp-size 4 \
-    --dist-timeout 60 \
-    --log-interval 50 \
-    --save-interval 5000 \
-    --eval-interval 5000 \
-    --report-to wandb \
-    --wandb-project specforge-qwen3-coder \
-    --wandb-name qwen3-coder-30b-eagle3-tp4-opc-train
+    --tp-size $TP_SIZE \
+    --target-model-backend sglang
