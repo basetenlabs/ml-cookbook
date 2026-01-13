1ddd9bb7de63c6be44bfe80d12b16ebf874c8032
Aghilan Nathan <aghilan.nathan@baseten.co>
2026-01-13 17:34:42 -0500

    feat(recipies): nemotron3 recipie

--- examples/nemotron3-30b-torchtitan/run.sh
+++ examples/nemotron3-30b-torchtitan/run.sh
@@ -36,10 +36,14 @@ NGPU=8 CONFIG_FILE="./torchtitan/experiments/nemotron3/train_configs/nemotron3-n
     ./run_train.sh \
     --checkpoint.initial_load_path "$BT_TEAM_CACHE_DIR/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16" \
     --model.hf_assets_path "$BT_TEAM_CACHE_DIR/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16" \
-    --checkpoint.interval 100 \
+    --checkpoint.interval 500 \
     --lr_scheduler.warmup_steps 10 \
-    --training.steps 100 \
     --job.dump_folder "$BT_CHECKPOINT_DIR" \
     --training.dataset "hf://HuggingFaceFW/fineweb" \
     --training.dataset_config_name "sample-10BT" \
-    --training.dataset_split "train"
+    --training.dataset_split "train" \
+    --training.local_batch_size 1 \
+    --training.seq_len 2048 \
+    --training.max_norm 1.0 \
+    --training.steps 500 \
+    --training.dtype "bfloat16"
