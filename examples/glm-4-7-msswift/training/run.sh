91469ace7792362755b0c4f45a60887ecff6b3bc
William Gao <william.gao@baseten.co>
2026-01-18 12:18:06 -0500

    increased seq length by increasing ep

--- examples/glm-4-7-msswift/training/run.sh
+++ examples/glm-4-7-msswift/training/run.sh
@@ -18,7 +18,7 @@ PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True NPROC_PER_NODE=$BT_NUM_GPUS NNO
     --no_initialization false \
     --split_dataset_ratio 0.01 \
     --tensor_model_parallel_size 8 \
-    --expert_model_parallel_size 8 \
+    --expert_model_parallel_size 16 \
     --moe_permute_fusion true \
     --moe_grouped_gemm true \
     --moe_shared_expert_overlap true \
@@ -37,7 +37,7 @@ PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True NPROC_PER_NODE=$BT_NUM_GPUS NNO
     --lr_warmup_fraction 0.05 \
     --min_lr 1e-5 \
     --eval_interval 40 \
-    --max_length 16384 \
+    --max_length 131072 \
     --num_workers 8 \
     --dataset_num_proc 8 \
     --no_save_optim true \
